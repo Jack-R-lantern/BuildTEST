b7afc20799106c10fcd2f686b4ca902c6be0b0c1
jack-r-lantern <tjdfkr2421@gmail.com>
2021-05-24 16:07:24 +0900

    Test

--- app/src/main/java/BuildTEST/App.java
+++ app/src/main/java/BuildTEST/App.java
@@ -5,7 +5,7 @@ package BuildTEST;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Bye World!";
     }
 
     public static void main(String[] args) {

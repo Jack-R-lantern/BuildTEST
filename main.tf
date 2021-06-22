229b64a1e30a9c8ef8f094e3c6036dd8e4b03091
jack-r-lantern <tjdfkr2421@gmail.com>
2021-06-22 21:00:51 +0900

    TEST

--- main.tf
+++ main.tf
@@ -1,3 +1,33 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "GitOps-Terraform"
+    key = "./terraform.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+    aws_dynamodb_talble = "terraform_lock"
+  }
+}
+
+resource "aws_s3_bucket" "GitOps_Terraform" {
+  bucket = "GitOps-Terraform"
+  force-destroy = true
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_talble" "GitOps_dyanmo" {
+  hash_key = "LockID"
+  name = "terraform_lock"
+  read_capacity = 1
+  write_capacity = 1
+
+  atrribute {
+    name = "LockID"
+    type = "S"
+  }
+}

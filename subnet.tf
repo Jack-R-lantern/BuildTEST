f3709c0f985fa9d1f9fdc0ad7e7d31e40ddf59ad
jack-r-lantern <tjdfkr2421@gmail.com>
2021-06-22 15:54:03 +0900

    TEST

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "GitOps_Public_a" {
   vpc_id = aws_vpc.GitOps_TEST.id
   cidr_block = "10.0.1.0/24"
-  availabilty_zone = "ap-northeast-2a"
+  availability_zone = "ap-northeast-2a"
   tags = {
     Name = "GitOps_Public_a"
   }

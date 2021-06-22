1ae615e757aa99708d3eebea1ed56fd539e84c9c
jack-r-lantern <tjdfkr2421@gmail.com>
2021-06-22 23:11:55 +0900

    change

--- main.tf
+++ main.tf
@@ -11,23 +11,3 @@ terraform {
     dynamodb_table = "terraform_lock"
   }
 }
-
-resource "aws_s3_bucket" "GitOps_Terraform" {
-  bucket = "gitops-terraform"
-  force-destroy = true
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_dynamodb_talble" "GitOps_dyanmo" {
-  hash_key = "LockID"
-  name = "terraform_lock"
-  read_capacity = 1
-  write_capacity = 1
-
-  atrribute {
-    name = "LockID"
-    type = "S"
-  }
-}
